912985b4eff7e9fbaa3d9edfe53f3a78d8b11120
Jamshid Khaksaar <jamshid.khaksaar@outlook.com>
2026-01-06 09:41:22 +0430

    Fix Modal Gray Screen Issue (Z-Index/Opacity)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -137,7 +137,7 @@
                  x-transition:leave="ease-in duration-200" 
                  x-transition:leave-start="opacity-100" 
                  x-transition:leave-end="opacity-0"
-                 class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" 
+                 class="fixed inset-0 bg-gray-500 bg-opacity-50 transition-opacity" 
                  @click="open = false"
                  aria-hidden="true"></div>
 
@@ -151,7 +151,7 @@
                  x-transition:leave="ease-in duration-200" 
                  x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100" 
                  x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                 class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg w-full">
+                 class="relative inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg w-full">
                 
                 <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                     <div class="sm:flex sm:items-start">
