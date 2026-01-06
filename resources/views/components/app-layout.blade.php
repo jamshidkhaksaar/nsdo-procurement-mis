256b8cabf9eba78c146c7404fa60153c3f190808
Jamshid Khaksaar <jamshid.khaksaar@outlook.com>
2026-01-06 10:50:30 +0430

    Make navbar sticky to the top

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -26,7 +26,7 @@
 <body class="bg-gray-100 font-sans antialiased">
     <div class="min-h-screen">
         <!-- Navbar -->
-        <nav class="bg-white border-b border-gray-200">
+        <nav class="bg-white border-b border-gray-200 sticky top-0 z-[40] shadow-sm">
             <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div class="flex justify-between h-16">
                     <div class="flex">
